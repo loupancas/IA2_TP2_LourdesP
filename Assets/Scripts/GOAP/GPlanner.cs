1b90af7b0af2fbed3fbbd26223ef758a053d5769
loupancas <129621626+loupancas@users.noreply.github.com>
2025-02-19 10:45:24 -0500

    cambio

--- Assets/Scripts/GOAP/GPlanner.cs
+++ Assets/Scripts/GOAP/GPlanner.cs
@@ -5,32 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class GPlanner
+public class GPlanner : MonoBehaviour
 {
     public event Action<IEnumerable<GAction>> OnPlanCompleted;
     public event Action OnCantPlan;
 
-    private const int _WATCHDOG_MAX = 200;
 
-    private int _watchdog;
+    private readonly List<Tuple<Vector3, Vector3>> _debugRayList = new List<Tuple<Vector3, Vector3>>();
 
-
-    public void Run(GState from, GState to, IEnumerable<GAction> actions,
-                    Func<IEnumerator, Coroutine> startCoroutine)
-    {
-        _watchdog = _WATCHDOG_MAX;
-
-        var astar = new AStar<GState>();
-        astar.OnPathCompleted += OnPathCompleted;
-        astar.OnCantCalculate += OnCantCalculate;
-
-        var astarEnumerator = astar.Run(from,
-                                        state => Satisfies(state, to),
-                                        node => Explode(node, actions, ref _watchdog),
-                                        state => GetHeuristic(state, to));
-
-        startCoroutine(astarEnumerator);
-    }
+ 
 
     public static FiniteStateMachine ConfigureFSM(IEnumerable<GAction> plan, Func<IEnumerator, Coroutine> startCoroutine)
     {
@@ -49,46 +32,12 @@ public class GPlanner
         return fsm;
     }
 
-    private void OnPathCompleted(IEnumerable<GState> sequence)
-    {
-        foreach (var act in sequence.Skip(1))
-        {
-            Debug.Log(act);
-        }
-
-        Debug.Log("WATCHDOG " + _watchdog);
-
-        var plan = sequence.Skip(1).Select(x => x.generatingAction);
-
-        OnPlanCompleted?.Invoke(plan);
-    }
-
-    private void OnCantCalculate()
-    {
-        OnCantPlan?.Invoke();
-    }
+  
 
     //private static float GetHeuristic(GState from, GState goal) => goal.state.Count(kv => !kv.In(from.state));
     //private static bool Satisfies(GState state, GState to) => to.state.All(kv => kv.In(state.state));
 
-    private static IEnumerable<WeightedNode<GState>> Explode(GState node, IEnumerable<GAction> actions,
-                                                                ref int watchdog)
-    {
-        if (watchdog == 0) return Enumerable.Empty<WeightedNode<GState>>();
-        watchdog--;
-
-        return actions//Where(action => action.preconditions.All(kv => kv.Inn(node.state)))
-                      .Aggregate(new List<WeightedNode<GState>>(), (possibleList, action) =>
-                      {
-                          var newState = new GState(node);
-                          //newState.state.UpdateWith(action.effects);
-                          newState.generatingAction = action;
-                          newState.step = node.step + 1;
-
-                          possibleList.Add(new WeightedNode<GState>(newState, action.Cost));
-                          return possibleList;
-                      });
-    }
+ 
 
     private void Check(Dictionary<string, bool> state, ItemType type)
     {
@@ -97,7 +46,7 @@ public class GPlanner
         var inventories = Navigation.instance.AllInventories();
         var floorItems = items.Except(inventories);//devuelve una coleccion como la primera pero removiendo los que estan en la segunda
         var item = floorItems.FirstOrDefault(x => x.type == type);
-        //var here = transform.position;
+        var here = transform.position;
         state["accessible" + type.ToString()] = item != null && Navigation.instance;
 
         var inv = inventories.Any(x => x.type == type);
@@ -182,11 +131,11 @@ public class GPlanner
 
 
         #region opcional
-        //var actDict = new Dictionary<string, ActionEntity>() {
-        //      { "Kill"  , ActionEntity.Kill }
-        //    , { "Pickup", ActionEntity.PickUp }
-        //    , { "Open"  , ActionEntity.Open }
-        //};
+        var actDict = new Dictionary<string, ActionEntity>() {
+              { "Kill"  , ActionEntity.Kill }
+            , { "Pickup", ActionEntity.PickUp }
+            , { "Open"  , ActionEntity.Open }
+        };
 
         #endregion
 
